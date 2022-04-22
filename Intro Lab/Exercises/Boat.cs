f2d842a7697b37fa508ae31eef05d86adc13bd46
Colin Herbst <colinherbst15@gmail.com>
2022-04-22 02:17:31 -0500

    CAC-102 Created Exercise 10

---
+++ Intro Lab/Exercises/Boat.cs
@@ -0,0 +1,44 @@
+﻿namespace Intro_Lab;
+/* This class contains my Exercise 10 lab for encapsulation */
+internal class Boat
+{
+    /*
+    Create a class based on your Boat object.
+    You will follow the same instructions as exercise 1 except for the following.
+    Include your attributes as Expression Body Definition Properties.
+    */
+
+    public int boatSize;
+    public string boatColor;
+    public string boatMaterial;
+
+    public int BoatSize
+    {
+        get => boatSize;
+        set => boatSize = value;
+    }
+    public string BoatColor
+    {
+        get => boatColor;
+        set => boatColor = value;
+    }
+    public string BoatMaterial
+    {
+        get => boatMaterial;
+        set => boatMaterial = value;
+    }
+
+    public Boat(int boatSize, string boatColor, string boatMaterial)
+    {
+        this.BoatSize = boatSize; 
+        this.BoatColor = boatColor;
+        this.BoatMaterial = boatMaterial;
+    }
+
+    public Boat(int boatSize, string boatColor)
+        : this(boatSize, boatColor, "Cloth") { }
+
+    public Boat()
+        : this(3,"White") { }
+
+} // end class
---
+++ Intro Lab/Exercises/Horse.cs
@@ -0,0 +1,36 @@
+﻿namespace Intro_Lab;
+/* This class contains my Exercise 10 lab for encapsulation */
+internal class Horse
+{
+    /*
+    Create a class based on your Horse object. Include your attributes as auto properties.
+    Create 3 constructors. The first constructor should have parameters equal to your properties.
+    Assign each parameter to your properties. The second constructor should have 2 parameters.
+    Using the :this() keyword, you will pass your information to the first constructor. 
+    This includes the 2 parameters and default values. For the last constructor,
+    you will create the default constructor. Using the :this()keyword, you will pass 2 default
+    values in that will pass to the 2nd constructor. Also include in the class, your method for Horse. 
+    */
+
+    public string HorseColor { get; init; }
+    public int HairLength { get; init; }
+    public int HorseWeight { get; init; }
+
+    public Horse(string horseColor, int hairLength, int horseWeight)
+    {
+        HorseColor = horseColor;
+        HairLength = hairLength;
+        HorseWeight = horseWeight;
+    }
+    public Horse(string horseColor, int hairLength)
+        :this(horseColor, 1 , hairLength) { }
+
+    public Horse()
+        :this("Black", 3) { }
+
+    public virtual void WakeUp()
+    {
+        Console.WriteLine("The horse wakes up");
+    }
+}
+
